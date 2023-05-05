e4d8d5a25210f86a3d38cfd7180d14f5afb2340a
ttipoya <124883294+ttipoya@users.noreply.github.com>
2023-05-05 09:12:07 +0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -2,71 +2,57 @@
 #На складе находятся компоненты разных компаний. Количество компаний К1, К2, … КN.
 #Сформировать все возможные варианты комплектации компьютеров.
 from itertools import product
+from random import randint
 print('Введите число число типов компонентов N: ',end = '')
 n = int(input())
 print('ЧАСТЬ 1')
 print('--------------------')
 firm = []
 uslov = []
-osnov = []
-s_uslov = []
+c = []
 kol = 0
 suma = 0
 rast = 0
-pods = 0
 maxi = 0
 maxikomb = ''
-minikomb =''
 for i in range(1, n + 1):
     firm.append('K' + str(i))
 for i in product(firm, repeat=n):
-    osnov.append(i)
-for i in osnov:
-   # print(i)
+    #print(i)
     kol = kol + 1
 print('Всего комбинаций:' + str(kol))
 print('--------------------')
 kol = 0
 print('ЧАСТЬ 2.1')
-print('Допустим у нас есть список всех компонентов и дано условие, что компоненты должны быть фирмы стоящей на нечётном месте')
+print('Допустим у нас есть список всех компаний и дано условие, что компоненты должны быть компаний к которым больше всего доверия')
 print('--------------------')
 for i in range(len(firm)):
     if int(firm[i][-1]) % 2 != 0:
         uslov.append(firm[i])
 for i in product(uslov, repeat=n):
-    s_uslov.append(i)
-for i in s_uslov:
- #   print(i)
+    #print(i)
     kol = kol + 1
 print('Всего комбинаций при новом условии:' + str(kol))
 print('--------------------')
 print('ЧАСТЬ 2.2')
-print('Допустим у нас есть список всех вариантов и дана задача найти максимальоне расстояние\n'
-'между элементами у которых на чётных местах стоят чётные компененты компаний, а на нечётных нечётные')
+print('Допустим фирму попросили собрать максимально дорогой компьютер так, \n'
+      'чтобы количество одинаковых компаний не превышало 2')
 print('--------------------')
-if n > 1:
-    for i in range(len(osnov)):
-        rast += 1
-        for m in range(0,n):
-            if int(osnov[i][m][-1]) %2 == 0 and m % 2 != 0:
-                suma = suma + 1
-            if int(osnov[i][m][-1]) % 2 != 0 and m % 2 == 0:
-                suma = suma + 1
-            if suma == n:
-                if pods == 0:
-                    pods+=1
-                elif pods == 1:
-                    if maxi <= rast:
-                        maxi = rast
-                        maxikomb = osnov[i]
-                        minikomb = osnov[i - rast]
-                        pods = 0
-                rast = 0
-        suma = 0
-    print('Максимальное расстояние:',maxi)
-    print('Первая комбинация:',minikomb)
-    print('Вторая комбинация:',maxikomb)
-    print('--------------------')
-else:
-    print("Число N слишком мало для поиска расстояния")
+print('Цены на компоненты компаний:')
+for i in range(1,n+1):
+    c.append(randint(10000,20000))
+    print('K'+str(i) + '='+ str(c[i-1]),end=' ')
+print('')
+print('--------------------')
+for i in product(firm, repeat=n):
+    for m in range(0,n):
+        for j in range(1,n+1):
+            if int(i[m][-1]) == j and i.count(i[m])<=2:
+                suma = suma + c[j-1]
+    if suma > maxi:
+        maxi = suma
+        maxikomb = i
+    suma= 0
+print('Маскимальная цена:',maxi)
+print('Комбинация:',maxikomb)
 
